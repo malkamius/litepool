278098f238812b96575bba71f13614825096441f
malkamius <cking999@gmail.com>
2024-06-08 01:31:38 -0500

    Run waitress from python

--- main/webserver/wsgi.py
+++ main/webserver/wsgi.py
@@ -1,4 +1,13 @@
 from .flask_app import app
+import logging
 
 if __name__ == "__main__":
-    app.run()
+    from shared import load_secrets
+    secrets = load_secrets()
+
+    try:
+        from waitress import serve
+        logging.basicConfig(level=logging.DEBUG)
+        serve(app, host=secrets["http_listen_on"], port=secrets["http_port"], _quiet = False)
+    except ImportError:
+        print("Module 'waitress' is not available.")
